69220fa0d800c1fccebbeb429cbe691defaada52
Ali-Mohamud <93277692+Ali-Mohamud@users.noreply.github.com>
2022-04-26 04:24:06 +0300

    save and methods to credential

--- credential.py
+++ credential.py
@@ -12,42 +12,42 @@ class Credential:
         self.credential_name = credential_name
         self.credential_password = credential_password
 
-    # def save_credential(self):
+    def save_credential(self):
         
-    #     Credential.credential_list.append(self)
+        Credential.credential_list.append(self)
 
-    # @classmethod
-    # def generate_password(cls):
+    @classmethod
+    def generate_password(cls):
        
     
-    #     size = 8
+        size = 8
 
         
-    #     alphanum = string.ascii_uppercase + string.digits + string.ascii_lowercase
+        alphanum = string.ascii_uppercase + string.digits + string.ascii_lowercase
 
         
-    #     password = ''.join( choice(alphanum) for num in range(size) )
+        password = ''.join( choice(alphanum) for num in range(size) )
         
-    #     return password
+        return password
 
-    # @classmethod
-    # def display_credential(cls,password):
+    @classmethod
+    def display_credential(cls,password):
       
-    #     user_credential_list = []
+        user_credential_list = []
 
-    #     for credential in cls.credential_list:
-    #         if credential.user_password == password:
-    #             user_credential_list.append(credential)
+        for credential in cls.credential_list:
+            if credential.user_password == password:
+                user_credential_list.append(credential)
 
-    #     return user_credential_list
+        return user_credential_list
     
-    # @classmethod
-    # def credential_exist(cls, name):
+    @classmethod
+    def credential_exist(cls, name):
         
-    #     for credential in cls.credential_list:
-    #         if credential.credential_name == name:
-    #             return True
+        for credential in cls.credential_list:
+            if credential.credential_name == name:
+                return True
             
-    #     return False
+        return False
 
 
