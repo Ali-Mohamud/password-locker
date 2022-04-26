41f1f37bca751a8a510f8971b751f1ae4027fd21
Ali-Mohamud <93277692+Ali-Mohamud@users.noreply.github.com>
2022-04-26 04:35:14 +0300

    create user list

--- user.py
+++ user.py
@@ -1,15 +1,15 @@
 from credential import Credential
 
 
-# class User:
+class User:
   
 
-#     user_list = []
+    user_list = []
 
-#     def __init__(self, user_name, user_password):
+    def __init__(self, user_name, user_password):
 
-#         self.user_name = user_name
-#         self.user_password = user_password
+        self.user_name = user_name
+        self.user_password = user_password
 
 #     def save_user(self):
 #         User.user_list.append(self)
